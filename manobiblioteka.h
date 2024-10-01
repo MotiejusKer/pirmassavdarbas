3d49234d5d5c9de45dbaa49072e5182e6f8330e4
MotiejusKer <motiejus.kerys@vvk.lt>
2024-10-01 21:37:29 +0300

    Create manobiblioteka.h
    header failas

---
+++ manobiblioteka.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+
+using std::string;
+
+using std::vector;
+
+using std::accumulate;
+using std::sort;
