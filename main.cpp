20de63816d41a2b1c05eb1ceb6c36555d167a1e4
MotiejusKer <motiejus.kerys@vvk.lt>
2024-10-26 11:32:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,17 @@
 
 int main()
 {
+    vector <studentas> eilutMas;
 
-	//
-	//A.printas();
-	vector <studentas> eilutMas;
-	//cout << eilutMas.size() << "   " << eilutMas.capacity()<<endl;
-	for (int i = 0; i < 2; i++)
-	{
-		string iv1, iv2; cout << "Iveskite varda ir pav.: ";
-		cin >> iv1 >> iv2;
-		vector <double> ivv;
-		int n;
-		for (int i = 0; i < 3; i++) {
-			cout << "iveskite namu darbu paz." << i + 1 << ": ";
-			cin >> n;
-			ivv.push_back(n);
-		}
-		cout << "Iveskite egzamino paz.: ";
-		cin >> n;
+    for (int i = 0; i < 2; i++) {
+        studentas A;
+        cin >> A;  
+        eilutMas.push_back(A);
+    }
 
-		studentas A(iv1, iv2, ivv, n);
-		eilutMas.push_back(A);
-		//cout << eilutMas.size() << "   " << eilutMas.capacity() << endl;
-	}
-	cout << endl;
-	for (auto a : eilutMas)
-		a.printas();
+    cout << endl;
+    for (const auto& a : eilutMas)
+        cout << a << endl;  
+
+    return 0;
+}
